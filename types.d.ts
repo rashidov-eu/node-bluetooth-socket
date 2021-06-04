717a48ac6704b645014f21ac5b6cd72a861c9eda
Will Ross <william.ross@gemalto.com>
2021-06-04 11:33:12 -0400

    type definition issues

--- types.d.ts
+++ types.d.ts
@@ -1,10 +1,8 @@
-declare module 'bluetooth-socket' {
-    import { Duplex, DuplexOptions } from 'stream';
-    class BluetoothSocket extends Duplex {
-        constructor(fd: number, options?: DuplexOptions);
-        bind(port: number);
-        listen(qlength: number = 1);
-        accept(options?: DuplexOptions, cb: (err, BluetoothSocket) => void);
-    }
-    export = BluetoothSocket;
+import { Duplex, DuplexOptions } from 'stream';
+class BluetoothSocket extends Duplex {
+    constructor(fd: number, options?: DuplexOptions);
+    bind(port: number): void;
+    listen(qlength?: number): void;
+    accept(options?: DuplexOptions, cb?: (err, BluetoothSocket) => void): void;
 }
+export = BluetoothSocket;
