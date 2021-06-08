d55d812ba408e29f5e63d634994957802d227986
Will Ross <william.ross@thalesgroup.com>
2021-06-08 15:59:32 -0400

    updated test2.js

--- test2.js
+++ test2.js
@@ -2,25 +2,61 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const BluetoothSocket = require(".");
+const { pipeline } = require('stream');
+const split = require('split');
+const through = require('through');
+
+function print(writeStream) {
+    return through(function write(data) {
+        writeStream.write(data + "\r\n");
+        this.emit('data', data);
+    })
+}
+
+function eol() {
+    return through(function write(data) {
+        this.emit('data', data + "\r\n");
+    })
+}
+
 class BluetoothController {
+    accept(server) {
+        setImmediate(() => {
+            console.log('-> accept');
+            server.accept(undefined, (err, socket) => {
+                if (err)
+                    return console.log(err);
+
+                console.log('|> socket opened');
+                socket.on('error', (err) => { console.error("~~>",err) });
+                socket.on('close', () => { 
+                    console.log('-> Close')
+                    this.accept(server) });
+                pipeline(
+                    socket,
+                    split(),
+                    print(process.stdout),
+                    eol(),
+                    socket,
+                    (err) => {
+                        console.error("~>",err);
+                    }
+                );
+            });
+        });
+    }
+
     listen() {
-        console.log('init');
+        console.log('-> init');
         const sock = new BluetoothSocket(0);
 
-        console.log('bind');
+        console.log('-> bind');
         sock.bind(22);
 
-        console.log('listen');
+        console.log('-> listen');
         sock.listen(1);
 
-        console.log('accept');
-        sock.accept(undefined, (err, socket) => {
-            if (err)
-                return console.log(err);
-            console.log('socket opened =>>');
-            socket.pipe(process.stdout);
-            socket.on('error', console.error);
-        });
+        this.accept(sock);
     }
 }
 const bt = new BluetoothController();
